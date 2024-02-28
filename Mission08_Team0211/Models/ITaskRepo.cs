b4396fdaf856a67c9774dc16a3319540e1016331
gracec123 <gracecwyu@gmail.com>
2024-02-28 16:54:26 -0700

    Add repo

---
+++ Mission08_Team0211/Models/ITaskRepo.cs
@@ -0,0 +1,12 @@
+﻿namespace Mission08_Team0211.Models
+{
+    public interface ITaskRepo
+    {
+        List<Category> Categories { get; }
+
+
+        void AddTask(MyTasks task);
+        void EditTask(MyTasks task);
+        void DeleteTask(int taskId);
+    }
+}
