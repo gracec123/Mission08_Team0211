f730b05ccc37d177d66034edf088e67d547aef5c
Tyler Sheffield <tylersheffield427@gmail.com>
2024-02-29 18:13:31 -0700

    fixed edit issues

--- Mission08_Team0211/Controllers/HomeController.cs
+++ Mission08_Team0211/Controllers/HomeController.cs
@@ -1,104 +1,87 @@
 using Microsoft.AspNetCore.Mvc;
 using Mission08_Team0211.Models;
-using System.Diagnostics;
 
-namespace Mission08_Team0211.Controllers
+namespace Mission08_Team0211.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ITaskRepo _context;
+
+    public HomeController(ITaskRepo temp)
     {
-        private readonly ITaskRepo _context;
+        _context = temp;
+    }
 
-        public HomeController(ITaskRepo temp)
+    public IActionResult Index()
+    {
+        // Display all tasks that have not been completed
+        var tasks = _context.AllTasks.Where(t => t.Completed != true).ToList();
+        return View(tasks);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        ViewBag.Categories = _context.Categories.ToList();
+        ViewBag.Title = "Create Task";
+
+        return View("AddTask", new AllTasks());
+    }
+
+    [HttpPost]
+    public IActionResult Create(AllTasks task)
+    {
+        if (ModelState.IsValid)
         {
-            _context = temp;
-        }
+            _context.AddTask(task);
 
-        public IActionResult Index()
-        {
-            // Display all tasks that have not been completed
-            var tasks = _context.AllTasks.Where(t => t.Completed != true).ToList();
-            return View(tasks);
-        }
-
-        [HttpGet]
-
-        public IActionResult Create()
-        {
-
-            ViewBag.Categories = _context.Categories.ToList();
-            return View("AddTask", new AllTasks());
-        }
-
-        [HttpPost]
-        public IActionResult Create(AllTasks task)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.AddTask(task);
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.Categories = _context.Categories;
-            return View("AddTask", task);
-        }
-
-
-        //[HttpPost]
-        //public IActionResult TaskList()
-        //{
-        //    var tasks = _context.AllTasks
-        //    .Where(t => t.Completed != true)
-        //    .ToList();
-
-        //    return View(tasks);
-        //}
-
-        [HttpGet]
-        public IActionResult Edit(int id)
-        {
-            var task = _context.AllTasks.FirstOrDefault(t => t.TaskId == id);
-            if (task == null)
-            {
-                return NotFound();
-            }
-
-            ViewBag.Categories = _context.Categories;
-            return View("AddTask", task); //
-        }
-
-        [HttpPost]
-        public IActionResult Edit(AllTasks task)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.EditTask(task);
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.Categories = _context.Categories;
             return RedirectToAction("Index");
         }
 
-        [HttpGet]
-        public IActionResult Delete(int id)
-        {
-            var task = _context.AllTasks.FirstOrDefault(t => t.TaskId == id);
-            if (task == null)
-            {
-                return NotFound();
-            }
+        ViewBag.Categories = _context.Categories;
 
-            return View("Index");
-        }
+        return View("AddTask", task);
+    }
 
-        [HttpPost, ActionName("Delete")] 
-        public IActionResult DeleteConfirmed(int id)
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var task = _context.AllTasks.FirstOrDefault(t => t.TaskId == id);
+        if (task == null) return NotFound();
+
+        ViewBag.Categories = _context.Categories;
+        ViewBag.Title = "Edit Task";
+        return View("AddTask", task); //
+    }
+
+    [HttpPost]
+    public IActionResult Edit(AllTasks task)
+    {
+        if (ModelState.IsValid)
         {
-            _context.DeleteTask(id);
+            _context.EditTask(task);
             return RedirectToAction("Index");
         }
 
+        ViewBag.Categories = _context.Categories;
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var task = _context.AllTasks.FirstOrDefault(t => t.TaskId == id);
+        if (task == null) return NotFound();
+
+        return View("Index");
+    }
+
+    [HttpPost]
+    [ActionName("Delete")]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        _context.DeleteTask(id);
+        return RedirectToAction("Index");
     }
 }
-
-
